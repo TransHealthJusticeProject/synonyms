7e04d0056bdcc3bfb42f0d117bb8e4107536b81b
Francisco Dans <fdansv@gmail.com>
2014-09-01 18:03:20 +0200

    adds url -> json parsing

--- src/java/com/mapzen/pelias/synonyms/Tools.java
+++ src/java/com/mapzen/pelias/synonyms/Tools.java
@@ -1,7 +1,43 @@
 package com.mapzen.pelias.synonyms;
 
-/**
- * Created by franciscodans on 01/09/2014.
- */
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
 public class Tools {
+    public static JSONObject getJSONFromURL(String urlString){
+        URL url = null;
+        String responseBody = "";
+        InputStream inputStream = null;
+        try {
+            url = new URL(urlString);
+            inputStream = url.openStream();
+            DataInputStream dataInputStream = new DataInputStream(new BufferedInputStream(inputStream));
+            String line;
+            while((line = dataInputStream.readLine()) != null){
+                responseBody += line;
+            }
+        }
+        catch (Exception e) { // For now this works for both IO and MalformedURL exceptions
+            e.printStackTrace();
+            System.exit(1);
+        }
+        finally {
+            try {
+                if (inputStream != null) {
+                    inputStream.close();
+                }
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            }
+        }
+        return new JSONObject(responseBody);
+
+    }
 }
