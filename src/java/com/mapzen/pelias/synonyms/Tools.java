62fcc6cf6660e8693e64e1d595e3ef9a07abe7da
Francisco Dans <fdansv@gmail.com>
2014-09-02 16:15:58 +0200

    adds HashMap generation from json datasource

--- src/java/com/mapzen/pelias/synonyms/Tools.java
+++ src/java/com/mapzen/pelias/synonyms/Tools.java
@@ -1,5 +1,6 @@
 package com.mapzen.pelias.synonyms;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.BufferedInputStream;
@@ -8,10 +9,15 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Tools {
-    public static JSONObject getJSONFromURL(String urlString){
+    public static JSONObject getJSONFromURL(String urlString) {
         URL url = null;
         String responseBody = "";
         InputStream inputStream = null;
@@ -39,5 +45,28 @@ public class Tools {
         }
         return new JSONObject(responseBody);
 
+    }
+
+    static String readFile(String path) throws IOException {
+        byte[] encoded = Files.readAllBytes(Paths.get(path));
+        return new String(encoded);
+    }
+
+    public static HashMap<String, ArrayList<String>> parseTerms (String path) throws IOException {
+        HashMap<String, ArrayList<String>> synMap = new HashMap<>();
+        JSONArray rootJSONArray = new JSONArray(readFile(path));
+        for (int i = 0; i<rootJSONArray.length(); i++){
+            JSONObject termJSON = rootJSONArray.getJSONObject(i);
+            Term term = new Term(termJSON);
+            if(!synMap.containsKey(term.fullWord)){
+                ArrayList<String> thisList = new ArrayList<String>();
+                thisList.add(term.abbreviation);
+                synMap.put(term.fullWord, thisList);
+            }
+            else{
+                synMap.get(term.fullWord).add(term.abbreviation);
+            }
+        }
+        return synMap;
     }
 }
