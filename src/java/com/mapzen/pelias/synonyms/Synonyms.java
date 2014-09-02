0a20e749b9c375256e0fc44dffb0336a2c360809
Francisco Dans <fdansv@gmail.com>
2014-09-02 16:14:33 +0200

    adds VERY basic command line functionality

--- src/java/com/mapzen/pelias/synonyms/Synonyms.java
+++ src/java/com/mapzen/pelias/synonyms/Synonyms.java
@@ -1,7 +1,49 @@
 package com.mapzen.pelias.synonyms;
 
-/**
- * Created by franciscodans on 01/09/2014.
- */
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class Synonyms {
+
+    public static void main(String[] arguments){
+        HashMap<String, ArrayList<String>> synMappings = new HashMap<>();
+        try {
+            synMappings = Tools.parseTerms("data/eng.json");
+        } catch (IOException e) {
+            System.out.println("There was a problem reading the file");
+            e.printStackTrace();
+            System.exit(1);
+        }
+        try {
+            PrintWriter writer = new PrintWriter("synonyms.txt", "UTF-8");
+            Iterator it = synMappings.entrySet().iterator();
+            while(it.hasNext()){
+                Map.Entry pair = (Map.Entry)it.next();
+                writer.print((pair.getKey() + " => "));
+                @SuppressWarnings("unchecked")
+                ArrayList<String> abbreviations = (ArrayList<String>)pair.getValue();
+                for(String abv: abbreviations){
+                    writer.print(abv);
+                    if(!(abbreviations.get(abbreviations.size()-1) .equals(abv))){
+                        writer.print(", ");
+                    }
+                }
+                writer.print("\n");
+            }
+            writer.close();
+            System.out.println("Cool! synonyms.txt has been generated in "+System.getProperty("user.dir"));
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            System.out.println("Wrong encoding");
+            e.printStackTrace();
+        }
+    }
 }
